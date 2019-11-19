6f77b6cffac16b626201f67f549a128f49151a7d
Christian Cury <contato.mikesantos@gmail.com>
2019-11-19 01:07:07 -0300

    feat: setup node cluster mode

---
+++ bin/fork.js
@@ -0,0 +1,38 @@
+import cluster from "cluster";
+import os from "os";
+
+const cpuCount = os.cpus().length;
+
+const App = {
+	init: async () => {
+		process.myEnv = {
+			customEnv: "Future"
+		};
+		return true;
+	}
+};
+
+App.init().then(() => {
+	cluster.setupMaster({ exec: "./bin/www" });
+
+	const forkCluster = () => {
+		const proc = cluster.fork();
+		proc.send(process.myEnv);
+	};
+
+	if (process.env.ENVIRONEMT === "development") {
+		forkCluster();
+	} else {
+		for (let i = 0; i < cpuCount; i++) {
+			forkCluster();
+		}
+	}
+
+	cluster.on("exit", (worker, code, signal) => {
+		console.warn(
+			`Worker: ${worker.process.pid} died with code: ${code}, and signal: ${signal}`
+		);
+		console.info("Starting a new worker");
+		forkCluster();
+	});
+});
